c4e6fcfb90b6692e71e6541aba299e77cafb094e
rasaighul <afc5865@psu.edu>
2023-04-14 13:34:48 -0400

    Thursday Session Work

--- src/progress-bar.js
+++ src/progress-bar.js
@@ -1,11 +1,12 @@
 import { LitElement, html, css } from 'lit';
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+import "./stopwatch-component.js";
 
 class ProgressBar extends LitElement {
   static properties = {
     title: { type: String },
     colorGradient: { type: String},
+    backgroundColor: {type: String},
 
 
   }
@@ -14,28 +15,56 @@ class ProgressBar extends LitElement {
       .wrapper{
         display: flex; 
         justify-content: center;
+        margin: 20px;
       }
       .progressBarStyling{
         border: 3px solid black;
-        border-radius: 1px;
+        border-radius: 10px;
+        height: 60px;
+        width: 450px;
+        margin-left: 40px;
+      }
+      .title{
+        margin-top: 20px; 
+        font-size: 20px;  
+      }
+      .backgroundProgress{
+        background: linear-gradient(to left, var(--background-of-bar, red), var(--background-of-bar, yellow));
+        border-radius: 5px;
         height: 50px;
-        width: 500px;
-        background-color: red;
+        width: 440px;
+        margin-left: 5px;
+        margin-top: 5px;
+        animation: backgroundProgress var(--speed-of-bar, 5s) linear forwards;
+      }
+
+      @keyframes backgroundProgress{
+        0% {width: 0%; }
+        100% {width: 97.8%; }
       }
   `;
 
   constructor() {
     super();
     this.title = "my will to live";
+    this.backgroundColor = "light gray";
     
   }
 
   render() {
     return html`
       
-      <div class='wrapper'> 
-      ${this.title} <div class='progressBarStyling'></div>
-      </div>
+  <div class='wrapper'> 
+        <div class='title'> 
+        ${this.title} 
+        </div>
+        <div class='progressBarStyling'>
+          <div class='backgroundProgress'>
+          </div>
+        <stopwatch-component>
+        </stopwatch-comonent>
+        </div>
+  </div>
 
     `;
   }
